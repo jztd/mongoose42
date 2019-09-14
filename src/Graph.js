319ee02acb868f3d6bf4ff28ddb1905276474db2
Tratast <ryan_c_brooks@hotmail.com>
2019-09-13 22:05:31 -0700

    Lots of things
    Moved data graphing to its own component.
    
    Styled some CSS.
    
    Changed graphical data updates to be more 'react-like'.

--- src/Graph.js
+++ src/Graph.js
@@ -1,10 +1,168 @@
 import React, { Component } from 'react';
-import Chart from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import RuneApi from './RunescapeApi';
+import './App.css';
+
 class Graph extends Component {
+    api = new RuneApi();
+
+    constructor(props) {
+        super(props);
+        this.state = { itemId: props.itemId, priceData: [], displayData: [], displayOptions: {}, selectedButton: "Year" };
+        this.postFix = '';
+    }
+
+    componentDidMount() {
+
+        this.api.getItemGraph(this.state.itemId).then(returnable => {
+            if (!returnable[0]) {
+                console.log("No pricing data was returned...");
+                return;
+            }
+            let formatData = [];
+            returnable.forEach(element => {
+                let time = new Date(element.date);
+                let price = element.daily;
+                formatData.push({x: time, y: price});
+            });
+            this.setState({priceData: formatData, displayData: this.formatData(formatData), displayOptions: this.createOptions(this.postFix, 'month')});
+        });
+    }
+
+    getArrayFromPrice = (data, axis) => {
+        return data.reduce((acc,element) => {
+            acc.push(element[axis]);
+            return acc;
+        },[]);
+    }
+
+    formatPriceArray = (arr) => {
+        //Determine the average price
+        let avg = arr.reduce((acc,element) => {
+            acc += element;
+            return acc;
+        });
+        avg /= arr.length;
+
+        if (avg > 1000000000) {
+            this.postFix = 'B';
+        } else if (avg > 1000000) {
+            this.postFix = 'M';
+        } else if (avg > 1000) {
+            this.postFix = 'k';
+        }
+        return arr;
+    }
+
+    formatData = (data) => {
+        return {
+            labels: this.getArrayFromPrice(data, 'x'),
+            datasets: [{
+                label: 'Price',
+                backgroundColor: 'transparent',//This is the fill color from y = 0 to the data points
+                pointBackgroundColor: '#ff7f0e',//The fill color of the point
+                pointHoverRadius: 3,//Radius of the point when hovered
+                //pointBorderColor: '#fff',//The stroke color of the point
+                //pointHoverBackgroundColor: '#fff',//The hover fill color of the point
+                //pointHoverBorderColor: 'rgba(220,220,220,1)',//The hover stroke color of the point
+                borderColor: '#ff7f0e',//color of the border of the circles indicating datapoints?
+                data: this.formatPriceArray(this.getArrayFromPrice(data,'y'))
+            }]
+        }
+    }
+
+    createOptions = (postFix, timeScale) => {
+        return {
+            layout: {
+                padding: {
+                    left: 0,
+                    right: 30,
+                    top: 0,
+                    bottom: 0
+                }
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    gridLines: {
+                        drawBorder: false
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Date'
+                    },
+                    type: 'time',
+                    time: {
+                        tooltipFormat: 'MMM Do',
+                        unit: timeScale,
+                        unitStepSize: 1
+                    }
+                }],
+                yAxes: [{
+                    gridLines: {
+                        drawBorder: false
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Price (gp)'
+                    },
+                    ticks: {
+                        callback: function(label,index,labels) {
+                            if (postFix === 'B') {
+                                return `${label/1000000000}${postFix}`;
+                            } else if (postFix === 'M') {
+                                return `${label/1000000}${postFix}`;
+                            } else if (postFix === 'k') {
+                                return `${label/1000}${postFix}`;
+                            }
+                            return `${label}`;
+                        }
+                    }
+                }]
+            }
+        }
+    }
+
+    updateChart = (range) => {
+        let length = this.state.priceData.length;
+        let newPriceArray = [];
+        let timeScale = 'month';
+        switch (range) {
+            case "Quarter":
+                newPriceArray = this.state.priceData.slice(length-1-91);
+                break;
+            case "Month":
+                newPriceArray = this.state.priceData.slice(length-1-30);
+                timeScale = "week";
+                break;
+            case "Week":
+                newPriceArray = this.state.priceData.slice(length-1-7);
+                timeScale = "day";
+                break;
+            default:
+                newPriceArray = this.state.priceData;
+                break;
+        }
+        this.setState({displayData: this.formatData(newPriceArray), displayOptions: this.createOptions(this.postFix, timeScale), selectedButton: range})
+    }
+
     render() {
         return (
-            //Insert Chart Stuff Here
-            );
+            <div class="col-sm-9 float-right mt-5">
+                <div class="col-sm-12 graph-nav row justify-content-end btn-group pt-2">
+                    <button className={`btn ${this.state.selectedButton === "Year" ? "graph-nav-active" : "graph-nav"}`} id="Year" type="button" onClick={() => this.updateChart("Year")}>Year</button>
+                    <button className={`btn ${this.state.selectedButton === "Quarter" ? "graph-nav-active" : "graph-nav"}`} id="Quarter" type="button" onClick={() => this.updateChart("Quarter")}>Quarter</button>
+                    <button className={`btn ${this.state.selectedButton === "Month" ? "graph-nav-active" : "graph-nav"}`} id="Month" type="button" onClick={() => this.updateChart("Month")}>Month</button>
+                    <button className={`btn ${this.state.selectedButton === "Week" ? "graph-nav-active" : "graph-nav"}`} id="Week" type="button" onClick={() => this.updateChart("Week")}>Week</button>
+                    <button className={`btn ${this.state.selectedButton === "Day" ? "graph-nav-active" : "graph-nav"}`} id="Day" type="button">Day</button>
+                </div>
+                <div class="row" id="graph">
+                    <Line data={this.state.displayData} options={this.state.displayOptions}/>
+                </div>
+            </div>
+        );
     }
 }
 
--- src/Item.js
+++ src/Item.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
 import RuneApi from './RunescapeApi';
-import { Line } from 'react-chartjs-2';
-import { access } from 'fs';
-import { thisExpression } from '@babel/types';
+import Graph from './Graph';
 
 class Item extends Component {
     api = new RuneApi();
@@ -19,186 +17,32 @@ class Item extends Component {
 
     constructor(props) {
         super(props);
-        console.log("in item component" + this.props.itemName);
-        this.state = { item: "", priceData: [] };
-        this.postFix = '';
-        this.chartReference = {};
+        this.state = { item: "" };
     }
 
 
     componentDidMount () {
        
         this.api.getItemInfo(this.props.itemName).then(response => {
-            console.log("got response " + response);
             this.setState({item : response});
-            console.log(`Grabbing price information for ${this.state.item.name} (id #${this.state.item.id})`);
-            this.api.getItemGraph(this.state.item.id).then(returnable => {
-                if (!returnable[0]) {
-                    console.log("No pricing data was returned...");
-                    return;
-                }
-                console.log(`Got graphical data for ${this.state.item.name}!`);
-                let formatData = [];
-                returnable.forEach(element => {
-                    let time = new Date(element.date);
-                    let price = element.daily;
-                    formatData.push({x: time, y: price});
-                });
-                this.setState({priceData: formatData});
-            });
         });
         
-    }
-
-    getArrayFromPrice = (data, axis) => {
-        return data.reduce((acc,element) => {
-            acc.push(element[axis]);
-            return acc;
-        },[]);
-    }
-
-    formatPriceArray = (arr) => {
-        //Determine the average price
-        let avg = arr.reduce((acc,element) => {
-            acc += element;
-            return acc;
-        });
-        avg /= arr.length;
-
-        if (avg > 1000000000) {
-            this.postFix = 'B';
-        } else if (avg > 1000000) {
-            this.postFix = 'M';
-        } else if (avg > 1000) {
-            this.postFix = 'k';
-        }
-        return arr;
-    }
-
-    formatData = () => {
-        let priceData = this.state.priceData;
-        return {
-            labels: this.getArrayFromPrice(priceData, 'x'),
-            datasets: [{
-                label: 'Price',
-                backgroundColor: 'transparent',//This is the fill color from y = 0 to the data points
-                pointBackgroundColor: '#ff7f0e',//The fill color of the point
-                pointHoverRadius: 3,//Radius of the point when hovered
-                //pointBorderColor: '#fff',//The stroke color of the point
-                //pointHoverBackgroundColor: '#fff',//The hover fill color of the point
-                //pointHoverBorderColor: 'rgba(220,220,220,1)',//The hover stroke color of the point
-                borderColor: '#ff7f0e',//color of the border of the circles indicating datapoints?
-                data: this.formatPriceArray(this.getArrayFromPrice(priceData,'y'))
-            }]
-        }
-    }
-
-    createOptions = (postFix) => {
-        return {
-            legend: {
-                display: false
-            },
-            scales: {
-                xAxes: [{
-                    gridLines: {
-                        drawBorder: false
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Date'
-                    },
-                    type: 'time',
-                    time: {
-                        tooltipFormat: 'MMM Do',
-                        unit: 'month',
-                        unitStepSize: 1
-                    }
-                }],
-                yAxes: [{
-                    gridLines: {
-                        drawBorder: false
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Price (gp)'
-                    },
-                    ticks: {
-                        callback: function(label,index,labels) {
-                            if (postFix === 'B') {
-                                return `${label/1000000000}${postFix}`;
-                            } else if (postFix === 'M') {
-                                return `${label/1000000}${postFix}`;
-                            } else if (postFix === 'k') {
-                                return `${label/1000}${postFix}`;
-                            }
-                            return `${label}`;
-                        }
-                    }
-                }]
-            }
-        }
-    }
-
-    updateChart = (range) => {
-        let length = this.state.priceData.length;
-        let newPriceArray = [];
-        switch (range) {
-            case "Quarter":
-                newPriceArray = this.state.priceData.slice(length-1-91);
-                this.chartReference.chartInstance.options.scales.xAxes[0].time.unit = "month";
-                //console.log('New Price Array: ', newPriceArray);
-                break;
-            case "Month":
-                newPriceArray = this.state.priceData.slice(length-1-30);
-                this.chartReference.chartInstance.options.scales.xAxes[0].time.unit = "week";
-                break;
-            case "Week":
-                newPriceArray = this.state.priceData.slice(length-1-7);
-                this.chartReference.chartInstance.options.scales.xAxes[0].time.unit = "day";
-                break;
-            default:
-                newPriceArray = this.state.priceData;
-                this.chartReference.chartInstance.options.scales.xAxes[0].time.unit = "month";
-                break;
-        }
-        //console.log("The chart reference is: ", this.chartReference.chartInstance);
-        this.chartReference.chartInstance.config.data.datasets[0].data = this.formatPriceArray(this.getArrayFromPrice(newPriceArray, 'y'));
-        this.chartReference.chartInstance.config.data.labels = this.getArrayFromPrice(newPriceArray, 'x');
-        this.chartReference.chartInstance.update();
     }
 
     render() {
         const item = this.state.item;
         if(item){
-            if (this.state.priceData[0]) {
-                return (
-                    <div>
-                        <div>
-                            <p>{item.name}</p>
-                            <p>{item.description}</p>
-                            <img src={item.icon} alt={item.name} />
-                        </div>
-                        <div id={"graph"}>
-                            <button id="Year" type="button" onClick={() => this.updateChart("Year")}>Year</button>
-                            <button id="Quarter" type="button" onClick={() => this.updateChart("Quarter")}>Quarter</button>
-                            <button id="Month" type="button" onClick={() => this.updateChart("Month")}>Month</button>
-                            <button id="Week" type="button" onClick={() => this.updateChart("Week")}>Week</button>
-                            <button id="Day" type="button">Day</button>
-                            <div>
-                                <Line  ref={(reference) => this.chartReference = reference} data={this.formatData()} options={this.createOptions(this.postFix)}/>
-                            </div>
-                        </div>
-                    </div>
-                );
-            } else {
-                return (
+            return (
+                <div>
                     <div>
                         <p>{item.name}</p>
                         <p>{item.description}</p>
                         <img src={item.icon} alt={item.name} />
                     </div>
-                );
-            }
+                    <Graph itemId={item.id}/>
+                    <Graph itemId={21787}/>
+                </div>
+            );
         }
         return "";
     }
